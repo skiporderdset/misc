327e77d1c0ea4e178953dae9590950ef39c45441
skiporderdset <no.in.person150313@gmail.com>
2021-01-31 09:27:30 +0900

    first commit

---
+++ memo.sh
@@ -0,0 +1,7 @@
+ git init
+ git config user.name "skiporderdset"
+ git config user.email "no.in.person150313@gmail.com"
+ # git remote set-url origin github_skiporderdset:skiporderdset/misc.git
+ git remote add origin github_skiporderdset:skiporderdset/misc.git
+ git add .
+ git commit -m "first commit"
